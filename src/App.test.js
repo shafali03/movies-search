8b790c50214239640057b5003ae4f580bc3303bd
shafali03 <shaf03sm@gmail.com>
2020-06-04 08:12:01 +0100

    feat: Displaying title

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme'
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+test('render the title', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.find('h1').text()).toContain("Movie Search App")
+})
